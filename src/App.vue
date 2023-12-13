98f56f88875274d4876e2359e576a2229021e92c
yuosif <yuosifraed@gmail.com>
2023-12-13 20:52:15 +0300

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <UpBar/>
-  <ConditionalRindering type="0"/>
+  <UpBar />
+  <ConditionalRindering type="0" />
   <div class="friends">
     <h2>My friends</h2>
-    <ul v-for="(friend,index) in friends" :key="index">
-       <FriendContact
-       :name="friend.name"
-       :phone="friend.phone"
-       :email="friend.email"
-       :is-favorate="1"
-       @selectedFavoriate="(name)=>messAlert(name)"
-       >
-       </FriendContact>
-    </ul>
+    <div v-if="friends.length > 0">
+      <ul v-for="(friend, index) in friends" :key="index">
+        <FriendContact :friend="friend" @selectedFavoriate="(name) => messAlert(name)"
+          @selectedFriend="(id) => removeFried(id)">
+        </FriendContact>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -21,6 +19,7 @@ import { defineComponent } from 'vue';
 import UpBar from './components/UpBar.vue';
 import FriendContact from './components/FriendContact.vue';
 import ConditionalRindering from './components/ConditionalRindering.vue';
+import friend from './interface/friend';
 
 export default defineComponent({
   name: 'App',
@@ -29,27 +28,38 @@ export default defineComponent({
     FriendContact,
     ConditionalRindering
   },
-  data(){
+  data() {
     return {
-      friends:[
+      friends: [
         {
-          id:1,
-          name:'yoyo',
-          phone:'0775235765',
-          email:'yoyo'
+          id: 1,
+          name: 'yoyo',
+          phone: '0775235765',
+          email: 'yoyo',
+          isFav: true
         },
         {
-          id:2,
-          name:'ali',
-          phone:'0775235765',
-          email:'ali'
+          id: 2,
+          name: 'ali',
+          phone: '0775235765',
+          email: 'ali',
+          isFav: false
         },
-      ]
+      ] as friend[]
     }
   },
-  methods:{
-    messAlert(name : string){
+  methods: {
+    messAlert(name: string) {
       alert(`this is fav ${name}`);
+    },
+    removeFried(id: number) {
+      // Find the index of the friend with the specified 'id'
+      let indexToRemove = this.friends.findIndex(friend => friend.id === id);
+
+      // If the friend with 'id' is found, remove it
+      if (indexToRemove !== -1) {
+        this.friends.splice(indexToRemove, 1);
+      }
     }
 
   }
@@ -57,7 +67,7 @@ export default defineComponent({
 </script>
 
 <style>
-.friends{
+.friends {
   align-content: center;
   text-align: center;
 }
