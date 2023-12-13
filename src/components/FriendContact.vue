adf1207dceda535db45d289d05e0a86562bf5ea5
yuosif <yuosifraed@gmail.com>
2023-12-13 13:58:46 +0300

    completed

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,10 +1,10 @@
 <template>
     <li>
-        <h2>{{name}} {{ friendIsFavoraite === '1'? '(favoraite)':''}}</h2>
+        <h2 :class="{favoriate : friendIsFavoraite === '1'}">{{name}} <strong>{{ friendIsFavoraite === '1'? '(favoraite)':''}}</strong></h2>
         <button @click="toggleFavorite">{{(friendIsFavoraite === '1' ? 'un' : 'is')}} Favoraite</button>
         <button @click="toggleDetails">{{detailAreVisible ? 'hide' : 'show'}} Details</button>
         <ul v-if="detailAreVisible">
-            <li><strong>phone:</strong>{{phone }}</li>
+            <li :style="{ display: ['-webkit-box', '-ms-flexbox', 'flex'] }" ><strong>phone:</strong>{{phone }}</li>
             <li><strong>email:</strong>{{email }}</li>
         </ul>
     </li>
@@ -43,3 +43,8 @@ toggleFavorite(){
   
 })
 </script>
+<style scoped>
+.favoriate{
+  color: red;
+}
+</style>
