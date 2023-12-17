b1e7284482a0262ff1e7ddf2fc2ac3709923e1c2
Yuosif <yuosifraed@gmail.com>
2023-12-17 08:36:45 +0300

    if used component in more than parent component using Global  if used in on  parent component used local registration

---
+++ src/components/BaseBadge.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="badge" :class="classes">{{ caption }}</span>
+</template>
+
+<script>
+export default {
+  props: ['type', 'caption'],
+  computed: {
+    classes() {
+      return {
+        'badge--admin': this.type === 'admin',
+        'badge--author': this.type === 'author',
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.badge {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  border-radius: 30px;
+  background-color: #ccc;
+  color: #2e2e2e;
+}
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ActiveElement from './components/ActiveElement.vue';
-import KnowledgeBase from './components/KnowledgeBase.vue';
-import KnowledgeElement from './components/KnowledgeElement.vue';
-import KnowledgeGrid from './components/KnowledgeGrid.vue';
+
+import BaseBadge from './components/BaseBadge.vue';
+import BadgeList from './components/BadgeList.vue';
+
 
 const app = createApp(App);
+//Global Component
 
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+app.component('base-badge', BaseBadge);
+app.component('badge-list', BadgeList);
+
 
 app.mount('#app');
