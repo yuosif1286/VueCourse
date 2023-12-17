2275471d7354eed47be0b7b00ef23a1b156c2980
Yuosif <yuosifraed@gmail.com>
2023-12-17 05:33:45 +0300

    you can use this if you have event in parent component has more one component is children

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base  @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base ></knowledge-base>
   </div>
 </template>
 
@@ -32,7 +32,10 @@ export default {
     };
   },
   provide(){
-    return{topics:this.topics}
+    return{
+      topics:this.topics,
+      selectTopic:this.activateTopic
+    }
   },
   mounted(){
     setTimeout(() => {   
--- src/components/KnowledgeGrid.vue
+++ src/components/KnowledgeGrid.vue
@@ -6,7 +6,6 @@
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
-      @select-topic="$emit('select-topic', $event)"
     ></knowledge-element>
   </ul>
 </template>
