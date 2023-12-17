b9d3a760a02aeec4241613f5031d87769359a7be
Yuosif <yuosifraed@gmail.com>
2023-12-17 09:30:32 +0300

    create template for base card example to useful

--- src/components/BadgeList.vue
+++ src/components/BadgeList.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <base-card>
     <h2>Available Badges</h2>
     <ul>
       <li>
@@ -9,20 +9,13 @@
         <base-badge type="author" caption="AUTHOR"></base-badge>
       </li>
     </ul>
-  </section>
+  </base-card>
 </template>
 
 <style scoped>
 section h2 {
   margin: 0.5rem 0;
   color: #3a3a3a;
-}
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
 }
 ul {
   list-style: none;
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import App from './App.vue';
 
 import BaseBadge from './components/BaseBadge.vue';
 import BadgeList from './components/BadgeList.vue';
-
+import BaseCard  from "@/components/BaseCard.vue";
 
 const app = createApp(App);
 //Global Component
 
 app.component('base-badge', BaseBadge);
 app.component('badge-list', BadgeList);
+app.component('base-card', BaseCard);
 
 
 app.mount('#app');
