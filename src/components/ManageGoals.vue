06fcd71fe22649a108fd9a2fb7c1b8311ec68174
Yuosif <yuosifraed@gmail.com>
2023-12-20 11:56:49 +0300

    this way to use alert is useful

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -2,20 +2,33 @@
   <h2>Manage Goals</h2>
   <input  type="text" ref="goal">
   <button @click="setGoal">set Goal</button>
+  <error-alert v-if="inputIsInvalid">
+    <h2>input is invalid!</h2>
+    <p>please enter at least a few characters</p>
+    <button @click="confirmError">Close</button>
+  </error-alert>
 </template>
 
 <script lang="js">
+import ErrorAlert from "./ErrorAlert.vue";
+
 export default {
+  components: {ErrorAlert},
+  data(){
+    return{
+      inputIsInvalid:false
+    }
+  },
   methods:{
     setGoal(){
-      const enteredValue= this.$refs.goal;
+      const enteredValue= this.$refs.goal.value;
 
-      if (enteredValue === '')
-      {
-        alert('input must not be empty');
-      }
-
+      this.inputIsInvalid = enteredValue === '';
+    },
+    confirmError(){
+      this.inputIsInvalid=false;
     }
+
   }
 }
 </script>
