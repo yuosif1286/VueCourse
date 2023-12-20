f2477d3e079832c0ab0aba28a8fe934e6acfe422
Yuosif <yuosifraed@gmail.com>
2023-12-20 10:10:10 +0300

    completed

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,21 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text">
+  <input  type="text" ref="goal">
+  <button @click="setGoal">set Goal</button>
 </template>
+
+<script lang="js">
+export default {
+  methods:{
+    setGoal(){
+      const enteredValue= this.$refs.goal;
+
+      if (enteredValue === '')
+      {
+        alert('input must not be empty');
+      }
+
+    }
+  }
+}
+</script>
