60b81a76e6d28e8e66ad420fb9235f87a5ca0ac6
Yuosif <yuosifraed@gmail.com>
2023-12-24 12:16:41 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <the-form></the-form>
+  <learning-survey ></learning-survey>
+  <user-experiences ></user-experiences>
 </template>
 
 <script>
-import TheForm from './components/TheForm.vue';
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
 
 export default {
   components: {
-    TheForm
-  }  
-}
+    LearningSurvey,
+    UserExperiences,
+   },
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
+};
 </script>
 
 <style>
@@ -23,6 +42,5 @@ html {
 
 body {
   margin: 0;
-  background-color: #292929;
 }
 </style>
