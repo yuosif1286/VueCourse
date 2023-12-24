531090374627ccb5d8ab2fca4f9ef4c3d6620cf5
Yuosif <yuosifraed@gmail.com>
2023-12-25 02:13:05 +0300

    complete

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import {createRouter,createWebHistory} from 'vue-router'
-
+import teamsList from "@/components/teams/TeamsList.vue";
+import usersList from "@/components/users/UsersList.vue";
 const app = createApp(App)
 
  const router =createRouter({
      history: createWebHistory(),
-     routes:[]
+     routes:[
+         {path:'/teams',component:teamsList},
+         {path:'/users',component:usersList},
+     ]
  });
 
+app.use(router);
 app.mount('#app');
