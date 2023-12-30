903ca8460626cbd4960d2e97ca7da64f029ca0ff
Yuosif <yuosifraed@gmail.com>
2023-12-30 03:04:07 +0300

    complete

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="SaveChanges">Save Change</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,17 +13,38 @@ export default {
   components: {
     UserItem,
   },
+  data(){
+    return{
+      changesSaved:false
+    };
+  },
   inject: ['users'],
   methods:{
     confirmInput(){
       // navigation from code
       this.$router.push('/teams');
+    },
+    SaveChanges(){
+      this.changesSaved=true;
     }
   },
   beforeRouteEnter(to,from,next){
     console.log('usersList cmp beforeRouteEnter');
     console.log(to,from);
     next();
+  },
+  beforeRouteLeave(to,from,next){
+    console.log('usersList cmp  beforeLeave');
+    console.log(to,from);
+    if (!this.changesSaved){
+     const userWantToLeave= confirm('Are you sure? you got unsaved changes!');
+      next(userWantToLeave);
+    }
+    else
+    next();
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 };
 </script>
