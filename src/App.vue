6964406c2ce7897892201d023f50317ed5d26033
Yuosif <yuosifraed@gmail.com>
2023-12-31 03:57:39 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,16 @@
 <template>
-  <the-navigation></the-navigation>
-  <main>
-    <router-view></router-view>
-  </main>
-  <footer>
-    <router-view name="footer"></router-view>
-  </footer>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button>Add 1</button>
+  </base-container>
 </template>
 
 <script>
-import TheNavigation from './components/nav/TheNavigation.vue';
+import BaseContainer from './components/BaseContainer.vue';
 
 export default {
   components: {
-    TheNavigation,
-  },
-  data() {
-    return {
-      activePage: 'teams-list',
-      teams: [
-        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
-        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
-        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
-      ],
-      users: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
-        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
-        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
-        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
-      ],
-    };
-  },
-  provide() {
-    return {
-      teams: this.teams,
-      users: this.users,
-    };
-  },
-  methods: {
-
+    BaseContainer,
   },
 };
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import router from './router'
+import {createStore} from 'vuex';
+const store=createStore({
+    state(){
+        return{
+            counter:0,
+        };
+    }
+});
 
 const app = createApp(App)
-
-app.use(router);
+app.use(store);
 app.mount('#app');
