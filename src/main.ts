9cb161478e0efee4aba262322fcf990fc6bea0b5
Yuosif <yuosifraed@gmail.com>
2024-01-02 15:43:39 +0300

    complete

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,83 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import {createStore} from 'vuex';
-interface State {
+import {ActionContext, createStore, Store} from 'vuex';
+interface StateCounter {
     counter: number;
+}
+interface State{
     isLoggedIn:boolean;
 }
+const CounterModule =  {
+    actions: {
+        increment(context: ActionContext<StateCounter, StateCounter>) {
+            setTimeout(function () {
+                context.commit('increment');
+            }, 2000)
+        },
+        increase(context: ActionContext<StateCounter, StateCounter>, payload: any) {
+            context.commit('increase', payload);
+        },
+    },
+    getters: {
+        finalCount(state: StateCounter) {
+            return state.counter * 3;
+        },
+        normalCount(_: StateCounter, getters  : any) {
+            const finalCount = getters.finalCount;
 
-const store=createStore({
-    state: function () {
-        return {
-            counter: 0,
-            isLoggedIn : false
-        };
-    },
-    mutations:{
-        increment(state: State){
-            state.counter= 2 + state.counter;
-        },
-        increase(state,payload){
-            state.counter+=payload.value;
-        },
-        setAuth(state ,payload)
-        {
-            state.isLoggedIn =payload.isAuth;
-        }
-    },
-    actions:{
-      increment(context){
-          setTimeout(function (){
-              context.commit('increment');
-          },2000)
-      },
-        increase(context,payload){
-            context.commit('increase',payload);
-        },
-        login(context){
-          context.commit('setAuth',{isAuth:true})
-        } ,
-        logout(context){
-          context.commit('setAuth',{isAuth:false})
-        }
-    },
-    getters:{
-        finalCount(state){
-            return state.counter *3;
-        },
-        normalCount(_,getters){
-            const finalCount=getters.finalCount;
-
-            if (finalCount<0)
+            if (finalCount < 0)
                 return 0;
-            if (finalCount>100)
+            if (finalCount > 100)
                 return 100;
 
             return finalCount;
         },
-        userIsAuth(state){
+    },
+    mutations: {
+        increment(state: StateCounter) {
+            state.counter = 2 + state.counter;
+        },
+        increase(state: StateCounter, payload: any) {
+            state.counter += payload.value;
+        },
+    },
+    state() : StateCounter {
+        return {
+            counter: 0,
+        };
+    },
+};
+const AuthModule=
+    {
+    actions: {
+        login(context:any) {
+            context.commit('setAuth', {isAuth: true})
+        },
+        logout(context:any) {
+            context.commit('setAuth', {isAuth: false})
+        }
+    },
+    getters: {
+        userIsAuth(state :State) {
             return state.isLoggedIn;
         }
+    },
+    mutations:
+        {
+            setAuth(state:State, payload:any) {
+            state.isLoggedIn = payload.isAuth;
+        }
+    },
+    state() {
+        return {
+            isLoggedIn: false
+        };
+},};
+const store =createStore({
+   modules:{
+       numbers:CounterModule,
+       Auth:AuthModule
     },
 });
 
