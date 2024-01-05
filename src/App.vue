3e0c4fa2962fb5a36b6910b7a81f23ef2f49a6a0
Yuosif <yuosifraed@gmail.com>
2024-01-05 04:22:17 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,22 @@
   <section class="container">
     <h2>{{ user.uName }}</h2>
     <h2>{{ user.age }}</h2>
+    <h2>{{oneParam}}</h2>
+    <button @click="setNewAge">Change Age</button>
   </section>
 </template>
 
 <script setup>
 //import {ref} from "vue";
-import {ref,reactive,isRef,toRefs,isReactive} from "vue";
+import {ref,reactive} from "vue";
 
-  const uName= ref('Maximilian');
+
   const user= reactive({uName:'Maximilian',age:42});
-  setTimeout(function (){
-   // uName.value='yoyo'
-    user.uName='yoyo';
-    user.age=32;
-  },2000);
-console.log(isReactive(user),isRef(uName));
-
-const newUser=toRefs(user);
-console.log(newUser);
+  const oneParam=ref('soso');
+  function setNewAge(){
+    user.age=20;
+    oneParam.value='yoyo';
+  }
 </script>
 
 <style>
