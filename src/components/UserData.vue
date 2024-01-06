7d1c81f129eace0c4e5f4a7bae0e106a62cf2e3f
Yuosif <yuosifraed@gmail.com>
2024-01-06 09:59:00 +0300

    complete

---
+++ src/components/UserData.vue
@@ -0,0 +1,21 @@
+
+<template>
+<h2>{{fullName}}</h2>
+  <h2>{{age}}</h2>
+</template>
+
+<script >
+import {computed} from "vue";
+
+export default {
+  props:['firstName','lastName','age'],
+  setup(props){
+    const fullName= computed()
+
+    return{};
+  },
+};
+</script>
+<style scoped>
+
+</style>
