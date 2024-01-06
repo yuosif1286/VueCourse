5deac6142849157cf48848ef2445012d9fa40d9c
Yuosif <yuosifraed@gmail.com>
2024-01-06 07:17:46 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <section class="container">
-    <h2>{{ user.uName }}</h2>
+    <h2>{{ uName }}</h2>
     <h2>{{ user.age }}</h2>
     <h2>{{oneParam}}</h2>
     <button @click="setNewAge">Change Age</button>
+
+    <input type="text" placeholder="write your first name" @input="setFirstName">
+    <input type="text" placeholder="write your last name" @input="setLastName">
   </section>
 </template>
 
 <script setup>
 //import {ref} from "vue";
-import {ref,reactive} from "vue";
+import {ref,reactive,computed} from "vue";
 
+   const firstName=ref('');
+   const lasstName=ref('');
+
+   const uName=computed(function (){
+     return firstName.value + ' ' +lasstName.value;
+   });
 
   const user= reactive({uName:'Maximilian',age:42});
   const oneParam=ref('soso');
   function setNewAge(){
     user.age=20;
     oneParam.value='yoyo';
+  }
+
+  function setFirstName(event){
+    firstName.value=event.target.value;
+  }
+  function setLastName(event){
+    lasstName.value=event.target.value;
   }
 </script>
 
