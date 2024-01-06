ca6f8bbf9694cb83c32598d83b452fc20e87176f
Yuosif <yuosifraed@gmail.com>
2024-01-06 15:12:07 +0300

    complete

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -5,13 +5,28 @@
 </template>
 
 <script >
-import {computed,inject} from "vue";
+import {
+  computed,
+  inject,
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onUnmounted,
+    onBeforeUnmount
+} from "vue";
 
 export default {
   props:['firstName','lastName'],
   setup(props){
     const fullName= computed(function (){
       return props.firstName +' '+ props.lastName;
+    });
+    onBeforeMount(function (){
+      console.log('good friend');
+    });
+    onUpdated(function (){
+      console.log('good friend');
     });
    const user=inject('user')
     return{fullName,user};
