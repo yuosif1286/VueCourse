c9a55c98eefc722e82e3f3cf7dc84b13cf9d470d
Yuosif <yuosifraed@gmail.com>
2024-01-06 07:46:12 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <button @click="setNewAge">Change Age</button>
 
     <input type="text" placeholder="write your first name" v-model="firstName">
-    <input type="text" placeholder="write your last name" v-model="lasstName">
+    <input type="text" placeholder="write your last name" v-model="lastName">
 
 <!--    <input type="text" placeholder="write your first name" @input="setFirstName">
     <input type="text" placeholder="write your last name" @input="setLastName">-->
@@ -15,13 +15,13 @@
 
 <script setup>
 //import {ref} from "vue";
-import {ref,reactive,computed} from "vue";
+import {ref, reactive, computed, watch} from "vue";
 
    const firstName=ref('');
-   const lasstName=ref('');
+   const lastName=ref('');
 
    const uName=computed(function (){
-     return firstName.value + ' ' +lasstName.value;
+     return firstName.value + ' ' +lastName.value;
    });
 
   const user= reactive({uName:'Maximilian',age:42});
@@ -30,13 +30,24 @@ import {ref,reactive,computed} from "vue";
     user.age=20;
     oneParam.value='yoyo';
   }
+//watcher
+/*watch(user,function (newUser,OldUser){
+  console.log('this New user Age:  '+newUser.age);
+  console.log('this old user Age:  '+OldUser.age);
+});*/
 
+  watch([user,uName],function (newValues,oldValues){
+  console.log('this New user Age:  '+newValues[0].age);
+  console.log('this old user Age:  '+oldValues[0].age);
+  console.log('this old uName:  '+oldValues[1]);
+  console.log('this new uName:  '+newValues[1]);
+});
 //used
   function setFirstName(event){
     firstName.value=event.target.value;
   }
   function setLastName(event){
-    lasstName.value=event.target.value;
+    lastName.value=event.target.value;
   }
 </script>
 
